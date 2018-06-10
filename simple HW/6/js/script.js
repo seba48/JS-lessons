ab42d85e6d6a52dfc1e87e1f67bed3715bbbd8fc
seba48 <sebchik@gmail.com>
2018-06-10 17:20:36 +0300

    6 homework is finished

--- simple HW/6/js/script.js
+++ simple HW/6/js/script.js
@@ -4,8 +4,9 @@
         goods_value = document.getElementsByClassName('goods-value')[0],
         items_value = document.getElementsByClassName('items-value')[0],
         employers_value = document.getElementsByClassName('employers-value')[0],
-        discount_value = document.getElementsByClassName('discount-value'),
+        discount_value = document.getElementsByClassName('discount-value')[0],
         isopen_value = document.getElementsByClassName('isopen-value')[0],
+
 
         goods_item = document.getElementsByClassName('goods-item'),
         goods_btn = document.getElementsByTagName('button')[1],
@@ -14,7 +15,16 @@
         choose_item = document.querySelector('.choose-item'),
         time_value = document.querySelector('.time-value'),
         count_budget_value = document.querySelector('.count-budget-value'),
-        hire_employers_item = document.querySelectorAll('.hire-employers-item');
+        hire_employers_item = document.querySelectorAll('.hire-employers-item'),
+        discount_check = document.getElementById('discount'),
+        goods_1 = document.getElementById('goods_1'),
+        goods_2 = document.getElementById('goods_2'),
+        goods_3 = document.getElementById('goods_3'),
+        goods_4 = document.getElementById('goods_4'),
+        employers_1 = document.getElementById('employers_1'),
+        employers_2 = document.getElementById('employers_2'),
+        employers_3 = document.getElementById('employers_3'),
+        main_functions_div = document.getElementsByClassName('main-functions');
 
     let money,
         price;
@@ -62,38 +72,131 @@
         let time = time_value.value;
 
         if (time < 0) {
-                console.log('it can\'t be true');
-                mainList.open = false;
-            } else if (time > 8 && time < 20) {
-                console.log('time to work!');
-                mainList.open = true;
-            } else if (time < 24) {
-                mainList.open = false;
-                console.log('it\'s too late now')
-            } else if (time > 24) {
-                console.log('there are just 24 hours in the day');
-                mainList.open = false;
-            };
-            if(mainList.open == true){
-                isopen_value.style.backgroundColor = 'green';
-            }else{
-                isopen_value.style.backgroundColor = 'red';
-            }            
+            console.log('it can\'t be true');
+            mainList.open = false;
+        } else if (time > 8 && time < 20) {
+            console.log('time to work!');
+            mainList.open = true;
+        } else if (time < 24) {
+            mainList.open = false;
+            console.log('it\'s too late now');
+        } else if (time > 24) {
+            console.log('there are just 24 hours in the day');
+            mainList.open = false;
+        };
+        if (mainList.open == true) {
+            isopen_value.style.backgroundColor = 'green';
+            
+        } else {
+            isopen_value.style.backgroundColor = 'red';
+            goods_btn.disabled = true;
+            budget_btn.disabled = true;
+            employers_btn.disabled = true;
+        }
 
-    })
+
+    });
+
+
 
     budget_btn.addEventListener('click', () => {
-        count_budget_value.value = money/30;
+        count_budget_value.value = money / 30;
     });
 
     employers_btn.addEventListener('click', () => {
         for (var i = 0; i < hire_employers_item.length; i++) {
-                let name = hire_employers_item[i].value;
-                mainList.employers[i] = name;
+            let name = hire_employers_item[i].value;
+            mainList.employers[i] = name;
 
-                employers_value.textContent += mainList.employers[i] + ', ';
-            }
-    })
+            employers_value.textContent += mainList.employers[i] + ', ';
+        }
+    });
+
+    discount_check.addEventListener('change', () => {
+        if (discount_check.checked) {
+            discount_value.style.backgroundColor = 'green';
+            mainList.discount = true;
+        } else {
+            discount_value.style.backgroundColor = 'red';
+            mainList.discount = false;
+        }
+    });
+
+    goods_1.addEventListener('change', () => {
+
+        if(mainList.open == true && goods_1.value != ''){
+            goods_btn.disabled = false;
+        }else if(goods_1.value == ''){
+            goods_btn.disabled = true;
+        }
+        
+    });
+    goods_2.addEventListener('change', () => {
+
+        if(mainList.open == true && goods_2.value != ''){
+            goods_btn.disabled = false;
+        }else if(goods_2.value == ''){
+            goods_btn.disabled = true;
+        }
+        
+    });
+    goods_3.addEventListener('change', () => {
+
+        if(mainList.open == true && goods_3.value != ''){
+            goods_btn.disabled = false;
+        }else if(goods_3.value == ''){
+            goods_btn.disabled = true;
+        }
+        
+    });
+    goods_4.addEventListener('change', () => {
+
+        if(mainList.open == true && goods_4.value != ''){
+            goods_btn.disabled = false;
+        }else if(goods_4.value == ''){
+            goods_btn.disabled = true;
+        }
+        
+    });
+
+    count_budget_value.addEventListener('change', () => {
+        if(mainList.open == true && count_budget_value.value != ''){
+            budget_btn.disabled = false;
+        }else {
+            budget_btn.disabled = true;
+        }
+    });
+
+    employers_1.addEventListener('change', () => {
+        if(mainList.open == true && employers_1.value != ''){
+            employers_btn.disabled = false;
+        }else {
+            employers_btn.disabled = true;
+        }
+    });
+    employers_2.addEventListener('change', () => {
+        if(mainList.open == true && employers_2.value != ''){
+            employers_btn.disabled = false;
+        }else {
+            employers_btn.disabled = true;
+        }
+    });
+    employers_3.addEventListener('change', () => {
+        if(mainList.open == true && employers_3.value != ''){
+            employers_btn.disabled = false;
+        }else {
+            employers_btn.disabled = true;
+        }
+    });
+
+
+
+
+
+
+
+
+
 
     const mainList = {
         budget: money,
@@ -105,7 +208,7 @@
         shopItems: [],
 
 
- 
+
 
         makeDiscount: function makeDiscount(discount, price) {
             if (discount == true) {
@@ -127,10 +230,16 @@
             for (let key in mainList) {
                 console.log('Our shop includes: ' + key);
             }
-        }
-
-
+        },
+         isOpen: function isOpen(open){
+            goods_btn.disabled = true;
+            budget_btn.disabled = true;
+            employers_btn.disabled = true;
+         }
 
 
 
     };
+
+mainList.isOpen();
+
