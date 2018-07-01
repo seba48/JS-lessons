124bac6bdded593a593d972c63eeeaa1489b6208
seba48 <sebchik@gmail.com>
2018-07-01 19:52:13 +0300

    AJAX is added but it does not work. will be fix tomorrow

--- simple HW/finish/window/js/script.js
+++ simple HW/finish/window/js/script.js
@@ -34,4 +34,48 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     // =====================================================================
+    // ==================  отправка форм по средствам AJAX  ================
+
+    let message = new Object();
+    message.loading = 'Загрузка...';
+    message.success = 'Спасибо, скоро мы с вами свяжемся!';
+    message.failur = 'что-то пошло не так(((';
+
+    let form = document.querySelector('.form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        //AJAX
+        let request = new XMLHttpRequest();
+        request.open("POST", 'server.php');
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        let formData = new FormData(form);
+
+        request.send(formData);
+
+        request.onreadystatechange = function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4) {
+                if (request.status == 200 && request.status < 300) {
+                    statusMessage.innerHTML = message.success;
+                    // Добавляем контент на страницу
+                } else {
+                    statusMessage.innerHTML = message.failur;
+                }
+            }
+        }
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+            // очищаем поля ввода
+        }
+    });
+
 });
